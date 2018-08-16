4ba8a7742fc5142bda22559d46c7c619d79cde8e
Myke <michael.mwebaze@gmail.com>
2018-08-16 10:58:07 -0400

    added new permissions

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # dmpa_dashboard_permissions
 
-Adds custom dashboard edit permission.
+Adds a permission that enables users to edit a page only if the country assigned to the page via the field_country matches one of the countries assigned to the user via the user profile.
--- src/DashboardEditPermissions.php
+++ src/DashboardEditPermissions.php
@@ -1,35 +1,48 @@
 <?php
-
-namespace Drupal\dmpa_dashboard_permissions;
-
-
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
-class DashboardEditPermissions {
-    use StringTranslationTrait;
-
-    /**
-     * Get permissions for Country Dashboards.
-     *
-     * @return array
-     *   Permissions array.
-     */
-    public function permissions(){
-        $countries = ['burkina-faso','drc','ghana','kenya','madagascar','malawi','mozambique', 'niger','nigeria',
-            'senegal','uganda','zambia'];
-
-        $permissions = [];
-
-        foreach ($countries as $country){
-            $permissions += [
-                'dashboard edit '.$country => [
-                    'title' => $this->t('Gives permissions to edit '.$country.' dashboard')
-                ]
-            ];
-        }
-        return $permissions;
-    }
-
-}
+//
+//namespace Drupal\dmpa_dashboard_permissions;
+//
+//
+//use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+//use Drupal\Core\StringTranslation\StringTranslationTrait;
+//use Symfony\Component\DependencyInjection\ContainerInterface;
+//
+//class DashboardEditPermissions {
+//
+//  use StringTranslationTrait;
+//
+//  /**
+//   * Get permissions for Country Dashboards.
+//   *
+//   * @return array
+//   *   Permissions array.
+//   */
+//  public function permissions() {
+//    $countries = [
+//      'burkina-faso',
+//      'drc',
+//      'ghana',
+//      'kenya',
+//      'madagascar',
+//      'malawi',
+//      'mozambique',
+//      'niger',
+//      'nigeria',
+//      'senegal',
+//      'uganda',
+//      'zambia',
+//    ];
+//
+//    $permissions = [];
+//
+//    foreach ($countries as $country) {
+//      $permissions += [
+//        'dashboard edit ' . $country => [
+//          'title' => $this->t('Gives permissions to edit ' . $country . ' dashboard'),
+//        ],
+//      ];
+//    }
+//    return $permissions;
+//  }
+//
+//}
